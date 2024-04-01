43fd1d756e49dba8dd7f497013c95401de8b82ab
Shreekar Halvi <shreekarhalvi1@gmail.com>
2024-04-02 00:42:49 +0530

    Initial Commit with Minimal Code

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use leptos::*;
+
+fn main() {
+    mount_to_body(|| view! { <App/> }) 
+}
+
+
+#[component]
+fn ProgressBar(
+    #[prop(default=100)]
+    max: u16,
+    #[prop(into)]
+    progress: Signal<i32>) -> impl IntoView {
+    view! {
+        <progress 
+            max=max
+            value=progress
+        />
+    }
+}  
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let double_count = move || count.get() * 2;
+    view! {
+        <button on:click=move |_| {set_count.update(|n| *n += 1);}>
+            "Click Me"
+        </button>
+
+        <ProgressBar progress=count/>
+        <ProgressBar progress=double_count/>
+    }
+}
